3c341086a9d4e13e3b5505dae72f165eb66c15c0
Wolzev <wolzev@mail.ru>
2019-01-25 02:19:03 +0500

    QRcode scanner

--- app/src/main/java/com/example/obir/intercom3/Intercom.kt
+++ app/src/main/java/com/example/obir/intercom3/Intercom.kt
@@ -1,18 +1,21 @@
 package com.example.obir.intercom3
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.KeyEvent
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.TextView
 import com.google.firebase.messaging.FirebaseMessaging
-
 
 class Intercom : AppCompatActivity() {
     var myWebView: WebView? = null
     var mySoundView: WebView? = null
+    var btn: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,14 +38,25 @@ class Intercom : AppCompatActivity() {
         myWebView!!.clearCache(true)
 
         val prefs = getSharedPreferences("com.example.obir.intercom3.PREFERENCE_FILE_KEY", Context.MODE_PRIVATE) ?: return
-        val urlVideo = prefs.getString("video_stream", "file:///assets/startpage.html")
-        val urlAudio = prefs.getString("audio_stream", "file:///assets/startpage.html")
+        val urlVideo = prefs.getString("video_stream", "file:///android_asset/startpage.html")
+        val urlAudio = prefs.getString("audio_stream", "file:///android_asset/startpage.html")
 
         myWebView!!.loadUrl(urlVideo)
         mySoundView!!.loadUrl(urlAudio)
 
+        tvresult = findViewById(R.id.tvresult) as TextView
+
+        btn = findViewById(R.id.btn) as Button
+
+        btn!!.setOnClickListener {
+            val intent = Intent(this@Intercom, ScanActivity::class.java)
+            startActivity(intent)
+        }
 
         FirebaseMessaging.getInstance().subscribeToTopic("all")
+    }
+    companion object {
+        var tvresult: TextView? = null
     }
     override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
         if (keyCode == KeyEvent.KEYCODE_BACK && myWebView!!.canGoBack()) {
